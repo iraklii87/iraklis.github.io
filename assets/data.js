07b80ddf0f7111640fb6f2d939659df13c5c7ecb
Your Name <ika.20028.ika@gmail.com>
2022-08-06 16:53:12 +0400

    dynamic data for portfolio

---
+++ assets/data.js
@@ -0,0 +1,34 @@
+const development = [
+    {
+        title: "Simple TiTle",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/9289f3113961907.60327ce77d954.png",
+        url: "https://www.facebook.com/ikaaXDD/",
+        urltext: "Facebook.com",
+    },
+    {
+        title: "Simple TiTle",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/98a38d113962557.60327fb94fb66.png",
+        url: "https://www.facebook.com/ikaaXDD/",
+        urltext: "Facebook.com",
+    }
+]
+
+
+const design = [
+    {
+        title: "Simple TiTle",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/9289f3113961907.60327ce77d954.png",
+        url: "https://www.facebook.com/ikaaXDD/",
+        urltext: "Facebook.com",
+    },
+    {
+        title: "Simple TiTle",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/98a38d113962557.60327fb94fb66.png",
+        url: "https://www.facebook.com/ikaaXDD/",
+        urltext: "Facebook.com",
+    }
+]
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,50 @@
 
+// <div class="protfolio-card">
+//     <img src="assets/img/work/1work.png" alt="" />
+//     <div class="p-3">
+//         <h4>WEB APP PAGE FOR TREVEL COMPANY</h4>
+//             <p>Do answered bachelor occasion in of offended no concerns. Supply worthy warmth branch of no ye. Voice tried known to as my to. Though wished merits. Do answered bachelor occasion in of offended no concerns. Supply worthy warmth branch of no ye. Voice tried known to as my to. Though wished merits ...</p>
+//         <a href="https://www.superapp.com/">www.superapp.com</a>
+//     </div>
+// </div>
+
+development.forEach(function(value){
+    $('.development-carousel').append(`
+    <div class="protfolio-card">
+        <img src="${value.img}" alt="" />
+        <div class="p-3">
+            <h4>${value.title}</h4>
+                <p>${value.desc}</p>
+            <a href="${value.url}">${value.urltext}</a>
+        </div>
+    </div>
+    `)
+ });
 
 
-    $('.owl-carousel').owlCarousel({
-        center: true,
-        items:1.2,
-        loop:true,
-        margin:16,
-        
-    })
+
+design.forEach(function(value){
+    $('.design-carousel').append(`
+    <div class="protfolio-card">
+        <img src="${value.img}" alt="" />
+        <div class="p-3">
+            <h4>${value.title}</h4>
+                <p>${value.desc}</p>
+            <a href="${value.url}">${value.urltext}</a>
+        </div>
+    </div>
+    `)
+});
+
+
+
+
+
+
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:1.2,
+    loop:true,
+    margin:16,
+    
+})
