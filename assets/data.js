336ac96518f8d80e740b3827232d7fa413a641aa
Your Name <ika.20028.ika@gmail.com>
2022-08-06 21:27:04 +0400

    data upload

--- assets/data.js
+++ assets/data.js
@@ -24,18 +24,88 @@ const development = [
 
 
 const design = [
+
     {
-        title: "Simple TiTle",
-        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
-        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/9289f3113961907.60327ce77d954.png",
-        url: "https://www.facebook.com/ikaaXDD/",
-        urltext: "Facebook.com",
+        title: "TBC Concept | Online Bank Redesign",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/projects/404/2f133a149443699.Y3JvcCw0MDM2LDMxNTcsOTksMA.png",
+        url: "https://www.behance.net/gallery/149443699/TBC-Concept-Online-Bank-Redesign",
+        urltext: "www.behance.com",
     },
+    
+
+    
     {
-        title: "Simple TiTle",
+        title: "Cosmohub's Landing page",
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
         img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/98a38d113962557.60327fb94fb66.png",
-        url: "https://www.facebook.com/ikaaXDD/",
-        urltext: "Facebook.com",
-    }
+        url: "https://www.behance.net/gallery/113962557/Cosmohubs-Landing-page",
+        urltext: "Behance.com",
+    },
+
+    {
+        title: "Group workspace, desktop style web page",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/9289f3113961907.60327ce77d954.png",
+        url: "https://www.behance.net/gallery/113961907/Group-workspace-desktop-style-web-page",
+        urltext: "www.behance.com",
+    },
+
+    {
+        title: "Skate Store | Web Design UI UX",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/81c117116055387.605a5ad514b45.png",
+        url: "https://www.behance.net/gallery/116055387/Skate-Store-Web-Design-UI-UX?tracking_source=project_owner_other_projects",
+        urltext: "www.behance.com",
+    },
+
+    {
+        title: "Instagram redesign (3 Pages)",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/5f7b50116238385.605db9020c19a.png",
+        url: "https://www.behance.net/gallery/116238385/Instagram-redesign-%283-Pages%29",
+        urltext: "www.behance.com",
+    },
+
+    {
+        title: "TBC Bank | Redesign",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/3a7aef125029011.6111036f5bb27.png",
+        url: "https://www.behance.net/gallery/125029011/TBC-Bank-Redesign",
+        urltext: "www.behance.com",
+    },
+
+    {
+        title: "Dota 2 Hero View Concept",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/9f5aae130329187.617da8a863600.png",
+        url: "https://www.behance.net/gallery/130329187/Dota-2-Hero-View-Concept",
+        urltext: "www.behance.com",
+    },
+
+    {
+        title: "STUDIO - ''ART FACTORY'' Intro Page Concept",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/2980be130422457.61800737e2e12.png",
+        url: "https://www.behance.net/gallery/130422457/STUDIO-ART-FACTORY-Intro-Page-Concept",
+        urltext: "www.behance.com",
+    },
+
+    {
+        title: "File.ge Redesign | App store Website",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/4ac760149443879.62e7e77350150.png",
+        url: "https://www.behance.net/gallery/149443879/Filege-Redesign-App-store-Website",
+        urltext: "www.behance.com",
+    },
+
+    {
+        title: "Dashboard Concept",
+        desc: "Cosmohub",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/57ca3c149444875.62e7ea57d796a.png",
+        url: "https://www.behance.net/gallery/149444875/Dashboard-Concept",
+        urltext: "www.behance.com",
+    },
+
+    
 ]
