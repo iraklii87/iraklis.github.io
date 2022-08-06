ccb435977f2b387b7f18a3a4f629853115dc3d87
Your Name <ika.20028.ika@gmail.com>
2022-08-06 19:02:59 +0400

    Changing Content data

--- assets/data.js
+++ assets/data.js
@@ -1,18 +1,25 @@
 const development = [
     {
-        title: "Simple TiTle",
+        title: "Goldstar-motors.ru | Car Dealing Website",
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
-        img: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/9289f3113961907.60327ce77d954.png",
-        url: "https://www.facebook.com/ikaaXDD/",
-        urltext: "Facebook.com",
+        img: "assets/img/work/1work.png",
+        url: "https://goldstarmotors.ru/",
+        urltext: "www.goldstarmotors.ru",
     },
     {
-        title: "Simple TiTle",
+        title: "Eagles.com",
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
-        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/98a38d113962557.60327fb94fb66.png",
-        url: "https://www.facebook.com/ikaaXDD/",
-        urltext: "Facebook.com",
-    }
+        img: "assets/img/work/3work.png",
+        url: "http://carseagle.com/",
+        urltext: "www.carseagle.com",
+    },
+    {
+        title: "Carwin | Car Dealing Website",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
+        img: "assets/img/work/2work.png",
+        url: "https://carwin.mypanel.ge/",
+        urltext: "www.carwin.ru",
+    },
 ]
 
 
